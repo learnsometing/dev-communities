632966c126c12b3f8bc2c513dd9527b84864561f
learnsometing <brianmonaccio@gmail.com>
2019-07-13 14:43:46 -0400

    Implement Post validations and default scope
    Wrote tests for Post that cover validations and the scope. Confirmed all
    tests pass.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  # Always see newest posts first
+  default_scope -> { order(created_at: :desc) }
+
+  validates :content, presence: true, length: { maximum: 3000 }
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,29 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @post = Post.new(content: 'This is my first post.')
+  end
+
+  test 'post with content should be valid' do
+    assert @post.valid?
+  end
+
+  test 'post should be invalid without content' do
+    @post.content = nil
+    assert @post.invalid?
+  end
+
+  test 'post content should be limited to 3000 characters' do
+    @post.content = 'a' * 3000
+    assert @post.valid?
+    @post.content += 'a'
+    assert @post.invalid?
+  end
+
+  test 'order should be most recent first' do  
+    assert_equal posts(:post_49), Post.first
+  end
 end
