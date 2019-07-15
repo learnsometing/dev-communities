036713ad9299638baf415e687bf8679d2f603d1c
learnsometing <brianmonaccio@gmail.com>
2019-07-15 16:26:12 -0400

    Add bootstrap classes to Passwords#new and Confirmations#new views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
-                                    passwords: 'users/passwords',
                                     registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
 end
