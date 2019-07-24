fc43c68e5a3630b00cc7158af31920e01b39236d
learnsometing <brianmonaccio@gmail.com>
2019-07-24 10:36:47 -0400

    Implement notifications for post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   # Associations
   belongs_to :author, class_name: 'User'
   has_many :notification_objects, as: :notification_triggerable, dependent: :destroy
-  
+
   # Validations
   validates :author_id, presence: true
   validates :content, presence: true, length: { maximum: 3000 }
@@ -14,4 +14,19 @@ class Post < ApplicationRecord
   # Always see newest posts first
   default_scope -> { order(created_at: :desc) }
 
+  after_create :send_notifications
+
+  private
+
+  def send_notifications
+    # Trigger the notification system after the creation of a post.
+    return if author.friends.empty?
+    
+    notification_object = notification_objects.create
+    notification_change = notification_object.notification_changes.create(actor_id: author_id)
+    author.friends.each do |friend|
+      notification_object.notifications.create(user_id: friend.id,
+                                               description: notification_change.full_description)
+    end
+  end
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -21,6 +21,7 @@ class PostTest < ActiveSupport::TestCase
   test 'post content should be limited to 3000 characters' do
     @post.content = 'a' * 3000
     assert @post.valid?
+
     @post.content += 'a'
     assert @post.invalid?
   end
@@ -30,5 +31,29 @@ class PostTest < ActiveSupport::TestCase
     create(:post, created_at: 10.minutes.ago)
     create(:post, created_at: 15.minutes.ago)
     assert_equal @post, Post.first
+  end
+
+  test 'post creation notficiations system' do
+    author = create(:confirmed_user)
+    friends = [create(:confirmed_user),
+               create(:confirmed_user),
+               create(:confirmed_user)]
+    author.friendships.create(friend: friends[0])
+    author.friendships.create(friend: friends[1])
+    author.friendships.create(friend: friends[2])
+
+    assert_equal friends[0].notifications.count, 0
+    assert_equal friends[1].notifications.count, 0
+    assert_equal friends[2].notifications.count, 0
+
+    assert_difference 'friends[0].notifications.count', 1 do
+      author.posts.create(attributes_for(:post))
+    end
+    assert_equal friends[1].notifications.count, 1
+    assert_equal friends[2].notifications.count, 1
+
+    assert_equal 3, Notification.count
+    assert_equal 1, NotificationChange.count
+    assert_equal 1, NotificationObject.count
   end
 end
